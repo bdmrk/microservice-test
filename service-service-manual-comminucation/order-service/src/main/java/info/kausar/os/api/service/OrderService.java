042d81a66dd35ffbe8a576fa52ca27ac7ca0becf
kausar <kausar@eatlbdd.net>
2021-03-15 11:48:32 +0600

    updated depency for service registry and keep a copy for manual comminucation

---
+++ service-service-manual-comminucation/order-service/src/main/java/info/kausar/os/api/service/OrderService.java
@@ -0,0 +1,37 @@
+package info.kausar.os.api.service;
+
+import info.kausar.os.api.common.Payment;
+import info.kausar.os.api.common.TransactionRequest;
+import info.kausar.os.api.common.TransactionResponse;
+import info.kausar.os.api.entity.Order;
+import info.kausar.os.api.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository oRepository;
+
+    @Autowired
+    private RestTemplate template;
+
+    public TransactionResponse orderSave(TransactionRequest request) {
+
+        String response = "";
+        Order order = request.getOrder();
+        Payment payment = request.getPayment();
+        payment.setOrderId(order.getId());
+        payment.setAmount(order.getPrice());
+        //rest api call
+
+        //for the time being hard code url
+        Payment paymentResponse = template.postForObject("http://192.168.1.118:9192/payment/doPayment", payment, Payment.class);
+        response = paymentResponse.getPaymentStatus().equals("success") ? "Payment success and order placed" : "Payment Failed but order placed";
+        oRepository.save(order);
+        return new TransactionResponse(order, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
+
+    }
+}
