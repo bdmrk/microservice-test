e35b38bc910ba39196bf8197627e35b029665658
kausar <kausar@eatlbdd.net>
2021-03-16 10:35:51 +0600

    updated

--- order-service/src/main/java/info/kausar/os/api/OrderServiceApplication.java
+++ order-service/src/main/java/info/kausar/os/api/OrderServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderServiceApplication {
@@ -18,5 +19,7 @@ public class OrderServiceApplication {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+
+
 
 }
